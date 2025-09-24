e91a607e5f4ea4cb4379d4832b1465069dc20ff8
Hevellyn <Celularhduarte@gmail.com>
2025-09-24 20:45:04 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string fraseDigitada, fraseComDislalia;
+
+Console.Write("Digite uma frase: ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseComDislalia = fraseDigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(fraseComDislalia);
